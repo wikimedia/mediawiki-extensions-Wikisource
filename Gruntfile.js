4a7cc0c02cd0957fc1a35b237a016211a24cc8bb
Tpt <thomaspt@hotmail.fr>
2017-08-24 18:30:12 +0200

    Adds all the MediaWiki extensions usual boilerplate
    Change-Id: Ie08909f4e20155649de7f95452b67f9d5f7a5d10

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/* eslint-env node, es6 */
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			all: [
+				'**/*.js',
+				'!node_modules/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'modules/**/*.css'
+			]
+		},
+		banana: conf.MessagesDirs,
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
---
+++ includes/Hooks.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace MediaWiki\Extension\Wikisource;
+
+class Hooks {
+}
