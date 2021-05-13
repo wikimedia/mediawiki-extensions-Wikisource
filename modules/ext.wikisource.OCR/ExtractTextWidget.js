73fbbf58aa56f8253a48644a183e998edec056e1
Sam Wilson <sam@samwilson.id.au>
2021-05-13 11:23:58 +0800

    Add OCR settings menu for choosing engine
    Add a settings menu to the floating OCR button that opens a popup
    window with an engine choice field.
    
    The chosen engine is saved to a new 'wikisource-ocr' LocalStorage JSON value.
    
    Also send the Content Language to the tool in the OCR request.
    
    Bug: T281769
    Change-Id: I5fe27ce29bada020160adb6564110178de5621b1

--- modules/ext.wikisource.OCR/ExtractTextWidget.js
+++ modules/ext.wikisource.OCR/ExtractTextWidget.js
@@ -7,23 +7,90 @@ var OcrTool = require( './OcrTool.js' );
  * @param {jQuery} $prpImage
  */
 function ExtractTextWidget( ocrTool, $prpImage ) {
-	var config = {
-		classes: [ 'ext-wikisource-ExtractTextWidget' ],
+	this.ocrTool = ocrTool;
+	this.prpImage = $prpImage.find( 'img' )[ 0 ].src;
+
+	var extractButton = new OO.ui.ButtonWidget( {
 		icon: 'ocr',
 		label: mw.message( 'wikisource-ocr-extract-text' ).text(),
 		title: mw.message( 'wikisource-ocr-extract-text-title' ).text()
+	} );
+	extractButton.$icon.addClass( 'ext-wikisource-icon-ocr' );
+	extractButton.connect( this, {
+		click: 'onClick'
+	} );
+
+	var configButton = new OO.ui.PopupButtonWidget( {
+		icon: 'expand',
+		title: mw.message( 'wikisource-ocr-settings-menu' ).text(),
+		invisibleLabel: true,
+		popup: {
+			anchor: false,
+			$content: this.getConfigContent().$element,
+			padded: false,
+			align: 'force-left'
+		}
+	} );
+
+	var config = {
+		classes: [ 'ext-wikisource-ExtractTextWidget' ],
+		items: [ extractButton, configButton ]
 	};
+
 	ExtractTextWidget.super.call( this, config );
-	this.$icon.addClass( 'ext-wikisource-icon-ocr' );
-	this.ocrTool = ocrTool;
-	this.$prpImage = $prpImage;
 }
 
-OO.inheritClass( ExtractTextWidget, OO.ui.ButtonWidget );
+OO.inheritClass( ExtractTextWidget, OO.ui.ButtonGroupWidget );
+
+ExtractTextWidget.prototype.getConfigContent = function () {
+	var radioSelect = new OO.ui.RadioSelectWidget( {
+			classes: [ 'ext-wikisource-ocr-engineradios' ],
+			items: [
+				new OO.ui.RadioOptionWidget( {
+					data: 'tesseract',
+					label: mw.msg( 'wikisource-ocr-engine-tesseract' )
+				} ),
+				new OO.ui.RadioOptionWidget( {
+					data: 'google',
+					label: mw.msg( 'wikisource-ocr-engine-google' )
+				} )
+			]
+		} ),
+		label = new OO.ui.LabelWidget( {
+			classes: [ 'ext-wikisource-ocr-engine-label' ],
+			label: mw.msg( 'wikisource-ocr-engine' ),
+			input: radioSelect
+		} );
+	radioSelect.connect( this, {
+		choose: 'onEngineChoose'
+	} );
+	radioSelect.selectItemByData( this.ocrTool.getEngine() );
+
+	this.advancedLink = new OO.ui.ButtonWidget( {
+		label: mw.msg( 'wikisource-ocr-advanced' ),
+		title: mw.msg( 'wikisource-ocr-advanced-title' ),
+		href: this.ocrTool.getUrl( false, this.prpImage ),
+		icon: 'linkExternal',
+		classes: [ 'ext-wikisource-ocr-advanced-link' ],
+		target: '_base'
+	} );
+
+	var content = new OO.ui.PanelLayout( {
+		padded: false,
+		expanded: false,
+		classes: [ 'ext-wikisource-ocr-config-panel' ]
+	} );
+	content.$element.append(
+		label.$element,
+		radioSelect.$element,
+		this.advancedLink.$element
+	);
+
+	return content;
+};
 
 ExtractTextWidget.prototype.onClick = function () {
-	var imgSrc = this.$prpImage.find( 'img' )[ 0 ].src,
-		url = this.ocrTool.getUrl( imgSrc, 'tesseract', [] ),
+	var url = this.ocrTool.getUrl( true, this.prpImage ),
 		textbox = document.querySelector( '#wpTextbox1' ),
 		extractTextWidget = this;
 	// Disable the UI.
@@ -62,6 +129,20 @@ ExtractTextWidget.prototype.processOcrResult = function ( response, textbox ) {
 		return;
 	}
 	textbox.value = response.text;
+};
+
+/**
+ * On changing the selected OCR engine.
+ *
+ * @param {OO.ui.OptionWidget} item Chosen item.
+ * @param {boolean} selected Item is selected.
+ */
+ExtractTextWidget.prototype.onEngineChoose = function ( item, selected ) {
+	if ( selected ) {
+		this.ocrTool.setEngine( item.data );
+		// Also update the advanced link's URL.
+		this.advancedLink.setHref( this.ocrTool.getUrl( false, this.prpImage ) );
+	}
 };
 
 module.exports = ExtractTextWidget;
--- modules/ext.wikisource.OCR/OcrTool.js
+++ modules/ext.wikisource.OCR/OcrTool.js
@@ -1,3 +1,5 @@
+var mwLocalStorage = require( 'mediawiki.storage' ).local;
+
 /**
  * @param {string} toolUrl
  * @class
@@ -5,21 +7,54 @@
  */
 function OcrTool( toolUrl ) {
 	this.toolUrl = toolUrl;
+	this.loadConfig();
 }
+
+OcrTool.prototype.saveConfig = function () {
+	mwLocalStorage.set( 'wikisource-ocr', JSON.stringify( {
+		engine: this.engine
+	} ) );
+};
+
+OcrTool.prototype.loadConfig = function () {
+	var config = JSON.parse( mwLocalStorage.get( 'wikisource-ocr' ) );
+	if ( config === null ) {
+		config = {};
+	}
+	if ( config.engine === undefined ) {
+		config.engine = 'tesseract';
+	}
+	this.engine = config.engine;
+};
+
+/**
+ * @param {string} engine
+ */
+OcrTool.prototype.setEngine = function ( engine ) {
+	this.engine = engine;
+	this.saveConfig();
+};
+
+/**
+ * @return {string}
+ */
+OcrTool.prototype.getEngine = function () {
+	return this.engine;
+};
 
 /**
  * Get the full URL to the OCR tool.
  *
+ * @param {boolean} api Whether to include the API endpoint.
  * @param {string} image URL of the image on Commons.
- * @param {string} engine Either 'google' or 'tesseract'.
- * @param {string[]} langs Array of language codes.
  * @return {string} Full URL to the tool.
  */
-OcrTool.prototype.getUrl = function ( image, engine, langs ) {
+OcrTool.prototype.getUrl = function ( api, image ) {
+	var endpoint = api ? '/api.php' : '/';
 	// @TODO Use URL() here when it's permitted in MediaWiki.
-	return this.toolUrl + '/api.php' +
-		'?engine=' + engine +
-		'&langs[]=' + langs.join( '&langs[]=' ) +
+	return this.toolUrl + endpoint +
+		'?engine=' + this.engine +
+		'&langs[]=' + mw.config.get( 'wgContentLanguage' ) +
 		'&image=' + encodeURIComponent( image ) +
 		'&uselang=' + mw.config.get( 'wgUserLanguage' );
 };
