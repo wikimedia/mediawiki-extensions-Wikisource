02a0e61146899838ccaf219455a70847aae6c19d
Sam Wilson <sam@samwilson.id.au>
2021-06-03 16:15:38 +0800

    Add loading indicator while OCR is running
    Turn the OcrTool class into the general model for this process,
    with ability to cancel etc.
    
    Bug: T282050
    Change-Id: Ibddc6fd906cc43b2f3e4d3a9175cd401f212d16c

--- modules/ext.wikisource.OCR/OcrTool.js
+++ modules/ext.wikisource.OCR/OcrTool.js
@@ -1,12 +1,34 @@
 var mwLocalStorage = require( 'mediawiki.storage' ).local;
 
 /**
- * @param {string} toolUrl
+ * This is the central model for the OCR process.
+ *
+ * @param {string} toolUrl Base URL for the OCR tool.
  * @class
  * @constructor
  */
 function OcrTool( toolUrl ) {
+	OO.EventEmitter.call( this );
+
+	// Event names.
+	this.events = {
+		cancelling: 'cancelling',
+		textExtractStart: 'textExtractStart',
+		textExtracted: 'textExtracted',
+		textExtractEnd: 'textExtractEnd'
+	};
+
+	// Base URL of the tool.
 	this.toolUrl = toolUrl;
+	// Engine name.
+	this.engine = 'tesseract';
+	// Array of language codes.
+	this.langs = [ mw.config.get( 'wgContentLanguage' ) ];
+	// Image URL.
+	this.image = null;
+	// Whether an OCR request is in progress.
+	this.inProgress = false;
+
 	this.loadConfig();
 }
 
@@ -42,21 +64,54 @@ OcrTool.prototype.getEngine = function () {
 	return this.engine;
 };
 
+OO.mixinClass( OcrTool, OO.EventEmitter );
+
 /**
  * Get the full URL to the OCR tool.
  *
  * @param {boolean} api Whether to include the API endpoint.
- * @param {string} image URL of the image on Commons.
  * @return {string} Full URL to the tool.
  */
-OcrTool.prototype.getUrl = function ( api, image ) {
+OcrTool.prototype.getUrl = function ( api ) {
 	var endpoint = api ? '/api.php' : '/';
 	// @TODO Use URL() here when it's permitted in MediaWiki.
 	return this.toolUrl + endpoint +
 		'?engine=' + this.engine +
-		'&langs[]=' + mw.config.get( 'wgContentLanguage' ) +
-		'&image=' + encodeURIComponent( image ) +
+		'&langs[]=' + this.langs.join( '&langs[]=' ) +
+		'&image=' + encodeURIComponent( this.image ) +
 		'&uselang=' + mw.config.get( 'wgUserLanguage' );
+};
+
+OcrTool.prototype.setImage = function ( image ) {
+	this.image = image;
+};
+
+OcrTool.prototype.cancel = function () {
+	this.inProgress = false;
+	this.emit( this.events.cancelling );
+};
+
+OcrTool.prototype.extractText = function () {
+	this.inProgress = true;
+	this.emit( this.events.textExtractStart );
+	var ocrTool = this;
+	$.ajax( {
+		url: ocrTool.getUrl( true ),
+		dataType: 'json',
+		success: function ( result ) {
+			if ( !ocrTool.inProgress ) {
+				// If the user has clicked 'cancel', ignore the result.
+				return;
+			}
+			ocrTool.emit( ocrTool.events.textExtracted, result );
+		},
+		error: function ( result ) {
+			ocrTool.emit( ocrTool.events.textExtracted, result );
+		},
+		complete: function ( jqXHR, textStatus ) {
+			ocrTool.emit( ocrTool.events.textExtractEnd, jqXHR, textStatus );
+		}
+	} );
 };
 
 module.exports = OcrTool;
